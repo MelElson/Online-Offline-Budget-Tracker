6876000fb98ee8da5f2a756e179a7dbc9debf0ed
Melanie <melanie.elson@icloud.com>
2021-06-30 21:46:28 -0600

    updateh

--- server.js
+++ server.js
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/stormy-crag", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+    userCreateIndex: true,  ///changed useCreateIndex
   useFindAndModify: false
 });
 
